bd2bd63fa432122e5fc5d04242461f1b690c5420
dawidg81 <dawidgorski.m@gmail.com>
2026-06-15 20:31:52 +0200

    Testing doxygen. main.c reindented

--- main.c
+++ main.c
@@ -3,42 +3,48 @@
 #include <time.h>
 #include <unistd.h>
 
+/**
+ * @brief A **main** function.
+ * @details
+ * The main function of dice.
+ * @return 0 if success.
+ */
 int main(int argc, char *argv[])
 {
-  int min = 1, max = 7;
-  int use_time = 0;
+	int min = 1, max = 7;
+	int use_time = 0;
 
-  for (int i = 0; i < argc; i++) {
-    if (argv[i][0] == '-' && argv[i][1] == 't') {
-      use_time = 1;
-    } else if (min == 1) {
-      min = atoi(argv[i]);
-    } else {
-      max = atoi(argv[i]);
-      break;
-    }
-  }
+	for (int i = 0; i < argc; i++) {
+		if (argv[i][0] == '-' && argv[i][1] == 't') {
+			use_time = 1;
+		} else if (min == 1) {
+			min = atoi(argv[i]);
+		} else {
+			max = atoi(argv[i]);
+			break;
+		}
+	}
 
-  int roll;
+	int roll;
 
-  if (use_time) {
-    srand(time(NULL));
-    roll = rand() % (max - min + 1) + min;
-  } else {
-    unsigned int random_value;
-    FILE* random = fopen("/dev/random", "r");
+	if (use_time) {
+		srand(time(NULL));
+		roll = rand() % (max - min + 1) + min;
+	} else {
+		unsigned int random_value;
+		FILE* random = fopen("/dev/random", "r");
 
-    if (random == NULL) {
-      perror("fopen");
-      return 1;
-    }
+		if (random == NULL) {
+			perror("fopen");
+			return 1;
+		}
 
-    fread(&random_value, sizeof(unsigned int), 1, random);
-    fclose(random);
+		fread(&random_value, sizeof(unsigned int), 1, random);
+		fclose(random);
 
-    roll = random_value % (max - min + 1) + min;
-  }
+		roll = random_value % (max - min + 1) + min;
+	}
 
-  printf("You've rolled a %d.\n", roll);
-  return 0;
+	printf("You've rolled a %d.\n", roll);
+	return 0;
 }
